29381397d57244a3c88dd7c1241f18ab3ffa4e7d
Devang <devangdhameliya5@gmail.com>
2025-11-11 17:54:43 +0530

    feat: Implement navigation to payment and lesson screens
    This commit enhances the action buttons on the course detail screen by implementing navigation logic.
    
    When a user clicks the "Start Learning" button:
    - If the course is premium and not yet purchased, the user is navigated to the `PaymentScreen` with the course details (ID, name, and price) passed as arguments.
    - If the course is free or already purchased, the user is navigated to the first lesson of that course.

--- lib/views/course/course_detail/widgets/action_buttons.dart
+++ lib/views/course/course_detail/widgets/action_buttons.dart
@@ -1,6 +1,8 @@
 import 'package:e_learning_application/models/course.dart';
+import 'package:e_learning_application/routes/app_routes.dart';
 import 'package:e_learning_application/services/dummy_data_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ActionButtons extends StatelessWidget {
   final Course course;
@@ -16,8 +18,21 @@ class ActionButtons extends StatelessWidget {
             onPressed: () {
               if (course.isPremium &&
                   !DummyDataService.isCourseUnlocked(course.id)) {
+                Get.toNamed(
+                  AppRoutes.payment,
+                  arguments: {
+                    'courseId': course.id,
+                    'courseName': course.title,
+                    'price': course.price,
+                  },
+                );
                 //payment screen
-              } else {}
+              } else {
+                Get.toNamed(
+                  AppRoutes.lesson.replaceAll(':id', course.lessons.first.id),
+                  parameters: {'courseId': course.id},
+                );
+              }
               ;
             },
             label: Text('Start Learning'),
@@ -27,9 +42,12 @@ class ActionButtons extends StatelessWidget {
         if (!course.isPremium ||
             DummyDataService.isCourseUnlocked(course.id)) ...[
           const SizedBox(width: 16),
-          IconButton(onPressed: () {
-            //chat screen
-          }, icon: const Icon(Icons.chat)),
+          IconButton(
+            onPressed: () {
+              //chat screen
+            },
+            icon: const Icon(Icons.chat),
+          ),
         ],
       ],
     );
